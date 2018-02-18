72904e88115172304d7ec5c86ce6588dc39c8bea
xur <xur@2345.com>
2018-02-18 19:13:35 +0800

    add ch09_3 second half

--- src/chapter09/ch09_3.kt
+++ src/chapter09/ch09_3.kt
@@ -1,5 +1,7 @@
 package chapter09
 
+import kotlin.reflect.KClass
+
 fun printContents(list: List<Any>) {
     println(list.joinToString())
 }
@@ -26,6 +28,47 @@ fun <T> copyData3(source: MutableList<T>,
         destination.add(item)
     }
 }
+
+fun printFirst(list: List<*>) {
+    if (list.isNotEmpty()) {
+        println(list.first())
+    }
+}
+
+fun <T> printFirst2(list: List<T>) {
+    if (list.isNotEmpty()) {
+        println(list.first())
+    }
+}
+
+interface FieldValidator<in T> {
+    fun validate(input: T): Boolean
+}
+
+object DefaultStringValidator : FieldValidator<String> {
+    override fun validate(input: String) = input.isNotEmpty()
+}
+
+object DefaultIntValidator : FieldValidator<Int> {
+    override fun validate(input: Int) = input >= 0
+}
+
+object Validators {
+    private val validators =
+            mutableMapOf<KClass<*>, FieldValidator<*>>()
+
+    fun <T: Any> registerValidator(
+            kClass: KClass<T>, fieldValidator: FieldValidator<T>) {
+        validators[kClass] = fieldValidator
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    operator fun <T: Any> get(kClass: KClass<T>): FieldValidator<T> =
+            validators[kClass] as? FieldValidator<T>
+                    ?: throw IllegalArgumentException(
+                    "No validator for ${kClass.simpleName}")
+}
+
 fun main(args: Array<String>) {
     printContents(listOf("abc", "bac"))
 
@@ -33,4 +76,16 @@ fun main(args: Array<String>) {
     val anyItems = mutableListOf<Any>()
     copyData(ints, anyItems)
     println(anyItems)
+
+    printFirst(listOf("Svetlana", "Dmitry"))
+
+    val validators = mutableMapOf<KClass<*>, FieldValidator<*>>()
+    validators[String::class] = DefaultStringValidator
+    validators[Int::class] = DefaultIntValidator
+
+
+    Validators.registerValidator(String::class, DefaultStringValidator)
+    Validators.registerValidator(Int::class, DefaultIntValidator)
+    println(Validators[String::class].validate("Kotlin"))
+    println(Validators[Int::class].validate(42))
 }
